2b21e6493ad24ec554e4558d212ff1eaa42089f9
zevdvlpr <zevdvlpr@gmail.com>
2021-04-20 19:23:59 -0300

    :bulb: chore: components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMM', {
+    locale: ptBR,
+  });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
+      <p>O Melhor para você ouvir</p>
+      <span>{currentDate}</span>
+    </header>
+  );
+}
