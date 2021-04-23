7e87944505f42c6934167d23a8dda63b0c1199e5
zevdvlpr <zevdvlpr@gmail.com>
2021-04-22 22:55:45 -0300

    add scroll bar to page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -27,33 +27,35 @@ interface EpisodeProps {
 
 export default function Episode({ episode }: EpisodeProps) {
   return (
-    <div className={styles.episode}>
-      <div className={styles.thumbnailContainer}>
-        <Link href="/">
+    <div className={styles.content}>
+      <div className={styles.episode}>
+        <div className={styles.thumbnailContainer}>
+          <Link href="/">
+            <button type="button">
+              <img src="/arrow-left.svg" alt="Voltar" />
+            </button>
+          </Link>
+          <Image
+            width={700}
+            height={160}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
           <button type="button">
-            <img src="/arrow-left.svg" alt="Voltar" />
+            <img src="/play.svg" alt="Tocar episódio" />
           </button>
-        </Link>
-        <Image
-          width={700}
-          height={160}
-          src={episode.thumbnail}
-          objectFit="cover"
+        </div>
+        <header>
+          <h1>{episode.title}</h1>
+          <span>{episode.members}</span>
+          <span>{episode.publishedAt}</span>
+          <span>{episode.durationAsString}</span>
+        </header>
+        <div
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: episode.description }}
         />
-        <button type="button">
-          <img src="/play.svg" alt="Tocar episódio" />
-        </button>
       </div>
-      <header>
-        <h1>{episode.title}</h1>
-        <span>{episode.members}</span>
-        <span>{episode.publishedAt}</span>
-        <span>{episode.durationAsString}</span>
-      </header>
-      <div
-        className={styles.description}
-        dangerouslySetInnerHTML={{ __html: episode.description }}
-      />
     </div>
   );
 }
@@ -67,7 +69,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     },
   });
 
-  const paths = data.map(episode => {
+  const paths = data.map((episode: any) => {
     return {
       params: {
         slug: episode.id,
